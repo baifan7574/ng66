34961526a7c28b8645ea3d39af01aca1a7104ebd
baifan7574 <baifan7574@gmail.com>
2025-07-27 22:21:50 +0800

    One.

--- ads.js
+++ ads.js
@@ -1,13 +1,59 @@
-var monetag = monetag || {};
-monetag.cmd = monetag.cmd || [];
-monetag.cmd.push(function() {
+// === Vignette Banner 1 (Anti-AdBlock)
+var s1 = document.createElement("script");
+s1.src = "https://a.monetag.com/fantastic-tag.min.js";
+s1.async = true;
+s1.onload = function() {
+  var monetag = monetag || {};
+  monetag.cmd = monetag.cmd || [];
+  monetag.cmd.push(function () {
     monetag.fantasticTag.init({
-        zoneId: 9632429,
+      zoneId: 9632429,
     });
-});
+  });
+};
+document.head.appendChild(s1);
 
-var s = document.createElement("script");
-s.type = "text/javascript";
-s.src = "https://a.monetag.com/fantastic-tag.min.js";
-s.async = true;
-document.head.appendChild(s);
+// === Vignette Banner 2
+var s2 = document.createElement("script");
+s2.src = "https://a.monetag.com/fantastic-tag.min.js";
+s2.async = true;
+s2.onload = function() {
+  var monetag = monetag || {};
+  monetag.cmd = monetag.cmd || [];
+  monetag.cmd.push(function () {
+    monetag.fantasticTag.init({
+      zoneId: 9632428,
+    });
+  });
+};
+document.head.appendChild(s2);
+
+// === In-Page Push 1 (Anti-AdBlock)
+var s3 = document.createElement("script");
+s3.src = "https://a.monetag.com/tag.min.js";
+s3.async = true;
+s3.onload = function() {
+  var monetag = monetag || {};
+  monetag.cmd = monetag.cmd || [];
+  monetag.cmd.push(function () {
+    monetag.inPagePushTag.init({
+      zoneId: 9632411,
+    });
+  });
+};
+document.head.appendChild(s3);
+
+// === In-Page Push 2
+var s4 = document.createElement("script");
+s4.src = "https://a.monetag.com/tag.min.js";
+s4.async = true;
+s4.onload = function() {
+  var monetag = monetag || {};
+  monetag.cmd = monetag.cmd || [];
+  monetag.cmd.push(function () {
+    monetag.inPagePushTag.init({
+      zoneId: 9632410,
+    });
+  });
+};
+document.head.appendChild(s4);
