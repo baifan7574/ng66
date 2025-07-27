aaeafede3dd2a77d57bc0da45830fbae056d901a
baifan7574 <baifan7574@gmail.com>
2025-07-27 21:27:47 +0800

    One, two.

--- ads.js
+++ ads.js
@@ -1,17 +1,19 @@
-(function () {
-  // 广告 1：Vignette banner（Anti-AdBlock）
-  var vignette = document.createElement("script");
-  vignette.src = "https://fpyf8.com/88/tag.min.js";
-  vignette.async = true;
-  vignette.setAttribute("data-zone", "9625896");  // Vignette Banner
-  vignette.setAttribute("data-cfasync", "false");
-  document.head.appendChild(vignette);
-
-  // 广告 2：In-Page Push（Anti-AdBlock）
-  var push = document.createElement("script");
-  push.src = "https://fpyf8.com/88/tag.min.js";
+// Monetag - In-Page Push
+(function() {
+  var push = document.createElement('script');
+  push.src = 'https://fpyf8.com/88/tag.min.js';
   push.async = true;
-  push.setAttribute("data-zone", "9625893");  // In-Page Push Banner
-  push.setAttribute("data-cfasync", "false");
-  document.body.appendChild(push);  // 放 body 避免冲突
+  push.setAttribute('data-zone', '9632410');
+  push.setAttribute('data-cfasync', 'false');
+  document.head.appendChild(push);
+})();
+
+// Monetag - Vignette Banner
+(function() {
+  var vignette = document.createElement('script');
+  vignette.src = 'https://fpyf8.com/88/tag.min.js';
+  vignette.async = true;
+  vignette.setAttribute('data-zone', '9632428');
+  vignette.setAttribute('data-cfasync', 'false');
+  document.head.appendChild(vignette);
 })();
