223dfaa410ff02347d5bc95342aa70c231bbc4e1
baifan7574 <baifan7574@gmail.com>
2025-07-27 21:41:45 +0800

    One.

--- ads.js
+++ ads.js
@@ -1,19 +1,21 @@
-// Monetag - In-Page Push
+<!-- ads.js 内容开始 -->
+// Monetag - In-Page Push (Anti-AdBlock)
 (function() {
   var push = document.createElement('script');
   push.src = 'https://fpyf8.com/88/tag.min.js';
   push.async = true;
-  push.setAttribute('data-zone', '9632410');
+  push.setAttribute('data-zone', '9632411');
   push.setAttribute('data-cfasync', 'false');
   document.head.appendChild(push);
 })();
 
-// Monetag - Vignette Banner
+// Monetag - Vignette Banner (Anti-AdBlock)
 (function() {
   var vignette = document.createElement('script');
   vignette.src = 'https://fpyf8.com/88/tag.min.js';
   vignette.async = true;
-  vignette.setAttribute('data-zone', '9632428');
+  vignette.setAttribute('data-zone', '9632429');
   vignette.setAttribute('data-cfasync', 'false');
   document.head.appendChild(vignette);
 })();
+<!-- ads.js 内容结束 -->
